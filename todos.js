2b034da1016900e70d996658fdf0dce03a8f6018
p <pawel.gitara@onet.eu>
2018-08-31 10:08:33 +0200

    saving to local storage done

---
+++ todos.js
@@ -0,0 +1,46 @@
+const todos 
+
+const filters = {
+    searchValue: ''
+}
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+////render Todos
+renderFilteredTodos(todos, filters)
+
+
+/// Input for filtering
+
+document.querySelector('#filter').addEventListener('input', (eventInfo) => {
+    filters.searchValue = eventInfo.target.value
+    renderFilteredTodos(todos, filters)
+})
+
+document.querySelector('#adder').addEventListener('submit', (eventInfo) => {
+   
+    eventInfo.preventDefault()
+    
+    todos.push({
+        title: eventInfo.target.elements.input1.value,
+        completed: false,
+        id: uuidv4()
+    })
+    
+    eventInfo.target.elements.input1.value=''
+    saveToStorage(todos)
+    renderFilteredTodos(todos,filters)
+})
